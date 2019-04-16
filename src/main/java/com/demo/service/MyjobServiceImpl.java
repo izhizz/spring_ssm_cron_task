96875d2fb7204261a1c123336c287cb10f3bc027
fengzhi <1336143962@qq.com>
2019-04-16 11:52:38 +0800

    单独quart定时器

---
+++ src/main/java/com/demo/service/MyjobServiceImpl.java
@@ -0,0 +1,10 @@
+package com.demo.service;
+
+import java.util.Date;
+
+public class MyjobServiceImpl {
+    public void execute() {
+        System.out.println(new Date());
+        System.out.println("aaaaaa");
+    }
+}
